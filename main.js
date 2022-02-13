08730427cc74613fe2ae8cd5d6d73997ea4d68e0
Haruna Alvin <harunaalvin69@gmail.com>
2022-02-13 23:08:05 +0100

    added operations for app

--- main.js
+++ main.js
@@ -105,3 +105,34 @@ const currentOperandTextElement = document.querySelector(
 );
 // --
 
+// Calculator Operations
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      calculator.appendNumber(button.innerText);
+      calculator.updateDisplay();
+    });
+  });
+  
+  operationButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      calculator.chooseOperation(button.innerText);
+      calculator.updateDisplay();
+    });
+  });
+
+  equalsButton.addEventListener('click', (button) => {
+    calculator.compute();
+    calculator.updateDisplay();
+  });
+
+  allClearButton.addEventListener('click', (button) => {
+    calculator.clear();
+    calculator.updateDisplay();
+  });
+
+  deleteButton.addEventListener('click', (button) => {
+    calculator.delete();
+    calculator.updateDisplay();
+  });
+  // --
